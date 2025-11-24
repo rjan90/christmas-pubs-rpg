67d7324d7de8d23704820b8d5432bdc555a20b8b
Phi-rjan <orjan.roren@gmail.com>
2025-11-24 17:19:00 +0100

    Add files via upload

---
+++ dist/tilesets/Door1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Door1" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/3 Animated Objects/Door1.png" width="192" height="64"/>
+</tileset>
---
+++ dist/tilesets/Hus.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Hus" tilewidth="116" tileheight="112" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="Hus.png" width="116" height="112"/>
+ </tile>
+</tileset>
