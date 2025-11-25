b88903008bb47fcb6aabbd6f4b15a1df28c0e51d
Phi <orjan.roren@gmail.com>
2025-11-25 14:52:36 +0100

    fix: update dist
    fix: update dist

---
+++ dist/tilesets/kok.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="kok" tilewidth="32" tileheight="32" tilecount="784" columns="28">
+ <image source="kok.png" width="896" height="896"/>
+</tileset>
